faacaaf3790cc705b54a73b6e54a26db93d68e8d
Hunt Tynch <tynchhunt@gmail.com>
2025-01-23 00:04:08 -0500

    Adding footer to webpage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Pacman from './components/Pacman'
-
 function App() {
 
   return (
     <>  
         <Pacman/>
         <Navbar/>
+        <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer () {
+  return (
+    <>
+    <div className="sticky h-24 light:text-gray-500 dark:text-gray-50 opacity-50">
+      <p className="italic font-medium font-mono">
+        Made by Hunt Tynch (2025)
+      </p>
+    </div>
+    </>
+  )
+}
